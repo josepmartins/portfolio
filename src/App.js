79be6fcfaa5757073dd19c6b539cc5eb0a95286a
Josep Martins <jsp.mrtns@gmail.com>
2018-06-04 14:17:20 +0200

    Steve corrections

--- src/App.js
+++ src/App.js
@@ -25,39 +25,39 @@ class App extends Component {
           </Text>
 
           <Text>
-            I'm Josep Martins, a digital product designer currently living in the sunny Barcelona.
+            I'm Josep Martins, a digital product designer currently living in sunny Barcelona.
           </Text>
 
           <Text>
-            I've never liked specializing in anything in particular and that's why I love learning and acquiring different skills in a self-taught way.
+            I've never liked specializing in anything in particular and that's why I love learning different skills in a self-taught way.
           </Text>
 
           <Text>
-            I work as a product designer at <a target='_blank' href='http://www.typeform.com'>Typeform</a>, improving the form creation experience and giving support to our design system continuous development.
+            I work as a product designer at <a target='_blank' href='http://www.typeform.com'>Typeform</a>, improving the form creation experience and developing our design system.
           </Text>
 
           <Text>
-            Before that, I worked in the cold Berlin as a designer at <a target='_blank' href='http://www.8fit.com'>8fit</a>, where I learned how different mobile products compared to the web could be.
+            Before that, I worked in cold Berlin as a designer at <a target='_blank' href='http://www.8fit.com'>8fit</a>, where I learned how to design for mobile products—and how much they can differ from the web.
           </Text>
 
           <Text>
-            My first job was at <a target='_blank' href='http://www.itnig.net'>itnig</a>, as a frontend developer in a startup accelerator where I worked on projects with totally different business and product needs.
+            My first job was at <a target='_blank' href='http://www.itnig.net'>itnig</a>, as a frontend developer in a startup accelerator. I worked on projects with totally different business and product needs.
           </Text>
 
           <Text>
-            One of those projects was <a target='_blank' href='http://www.playfulbet.com'>Playfulbet</a>, where I joined and worked for two years as frontend and designer.
+            One of those projects was <a target='_blank' href='http://www.playfulbet.com'>Playfulbet</a>, where I worked for two years as a frontend developer and designer.
           </Text>
 
           <Text>
-            We improved the structure, unified patterns and interaction while adding new functionalities. Always listening to the feedback of our users.
+            We improved the structure, unified patterns and interaction, and added new functionalities— while always listening to the feedback of our users.
           </Text>
 
           <Text>
-            So far one of the best experiences I have lived, being part of a successful project created from the scratch with only 4 people and a lot of enthusiasm.
+            It was one of the best experiences of my life, because I was part of a successful project created from scratch with only 4 people and a lot of enthusiasm.
           </Text>
 
           <Text>
-            If you would like to talk or to have a a coffee, contact me <a target='_blank' href='mailto:jsp.mrtns@gmail.com'>here</a>. You can also have a look to my pictures on <a target='_blank' href='http://www.instagram.com/josepmartins'>Instagram</a>.
+            If you'd like to talk or have a coffee, contact me <a target='_blank' href='mailto:jsp.mrtns@gmail.com'>here</a>. You can also take a look at my pictures on <a target='_blank' href='http://www.instagram.com/josepmartins'>Instagram</a>.
           </Text>
 
           <Text>
