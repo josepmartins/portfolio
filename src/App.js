059ecc5cbfbe3419d57e007a7ac92f97d5b98b57
josepmartins <jsp.mrtns@gmail.com>
2019-02-17 17:10:23 +0100

    Use rel="noopener" on target blank links

--- src/App.js
+++ src/App.js
@@ -35,19 +35,19 @@ class App extends Component {
           </Text>
 
           <Text>
-            I work as a product designer at <a target='_blank' href='http://www.typeform.com'>Typeform</a>, improving the form creation experience and developing our design system.
+            I work as a product designer at <a target='_blank' rel="noopener noreferrer" href='http://www.typeform.com'>Typeform</a>, improving the form creation experience and developing our design system.
           </Text>
 
           <Text>
-            Before that, I worked in cold Berlin as a designer at <a target='_blank' href='http://www.8fit.com'>8fit</a>, where I learned how to design for mobile products—and how much they can differ from the web.
+            Before that, I worked in cold Berlin as a designer at <a target='_blank' rel="noopener noreferrer" href='http://www.8fit.com'>8fit</a>, where I learned how to design for mobile products—and how much they can differ from the web.
           </Text>
 
           <Text>
-            My first job was at <a target='_blank' href='http://www.itnig.net'>itnig</a>, as a frontend developer in a startup accelerator. I worked on projects with totally different business and product needs.
+            My first job was at <a target='_blank' rel="noopener noreferrer" href='http://www.itnig.net'>itnig</a>, as a frontend developer in a startup accelerator. I worked on projects with totally different business and product needs.
           </Text>
 
           <Text>
-            One of those projects was <a target='_blank' href='http://www.playfulbet.com'>Playfulbet</a>, where I worked for two years as a frontend developer and designer.
+            One of those projects was <a target='_blank' rel="noopener noreferrer" href='http://www.playfulbet.com'>Playfulbet</a>, where I worked for two years as a frontend developer and designer.
           </Text>
 
           <Text>
@@ -59,7 +59,7 @@ class App extends Component {
           </Text>
 
           <Text>
-            If you'd like to talk or have a coffee, contact me <a target='_blank' href='mailto:jsp.mrtns@gmail.com'>here</a>. You can also take a look at my pictures on <a target='_blank' href='http://www.instagram.com/josepmartins'>Instagram</a>.
+            If you'd like to talk or have a coffee, contact me <a target='_blank' rel="noopener noreferrer" href='mailto:jsp.mrtns@gmail.com'>here</a>. You can also take a look at my pictures on <a target='_blank' rel="noopener noreferrer" href='http://www.instagram.com/josepmartins'>Instagram</a>.
           </Text>
 
         </Wrapper>
