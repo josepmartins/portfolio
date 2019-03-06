08ea9560c238f9c3a7a00b9810c6974e6de12d65
josepmartins <jsp.mrtns@gmail.com>
2019-03-06 19:48:01 +0100

    Simplify copies

--- src/App.js
+++ src/App.js
@@ -40,26 +40,6 @@ class App extends Component {
           </Text>
 
           <Text>
-            Before that, I worked in cold Berlin as a designer at <a target='_blank' rel="noopener noreferrer" href='http://www.8fit.com'>8fit</a>, where I learned how to design for mobile products—and how much they can differ from the web.
-          </Text>
-
-          <Text>
-            My first job was at <a target='_blank' rel="noopener noreferrer" href='http://www.itnig.net'>itnig</a>, as a frontend developer in a startup accelerator. I worked on projects with totally different business and product needs.
-          </Text>
-
-          <Text>
-            One of those projects was <a target='_blank' rel="noopener noreferrer" href='http://www.playfulbet.com'>Playfulbet</a>, where I worked for two years as a frontend developer and designer.
-          </Text>
-
-          <Text>
-            We improved the structure, unified patterns and interaction, and added new functionalities— while always listening to the feedback of our users.
-          </Text>
-
-          <Text>
-            It was one of the best experiences of my life, because I was part of a successful project created from scratch with only 4 people and a lot of enthusiasm.
-          </Text>
-
-          <Text>
             If you'd like to talk or have a coffee, contact me <a target='_blank' rel="noopener noreferrer" href='mailto:jsp.mrtns@gmail.com'>here</a>. You can also take a look at my pictures on <a target='_blank' rel="noopener noreferrer" href='http://www.instagram.com/josepmartins'>Instagram</a>.
           </Text>
 
